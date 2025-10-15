65a8560a5357b8e0e9aa53802f32379d3235ae19
Paraxial O'Kelly <para.okelly@nmtafe.wa.edu.au>
2025-10-15 15:55:26 +0800

    Initial FilmFinder commit

---
+++ FilmFinder/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using MySql.Data.MySqlClient;
+
+namespace FilmFinder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly MySql.Data.MySqlClient.MySqlConnection connection;
+        private readonly string dbServer = "127.0.0.1";
+        private readonly string dbPort = "3306";
+        private readonly string dbName = "sakila";
+        private readonly string dbUser = "sakila_app_user";
+        private readonly string dbPassword = "Hunter2!";
+        public MainWindow()
+        {
+            InitializeComponent();
+            connection = new MySql.Data.MySqlClient.MySqlConnection();
+            connection.ConnectionString = $"server={dbServer};port={dbPort};database={dbName};uid={dbUser};pwd={dbPassword}";
+
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            ErrorBox.Visibility = Visibility.Collapsed;
+            string clause = SearchBar.Text;
+            SearchBar.Text = "";
+
+            string query = $"select * from film where description like '%{clause}%'";
+            FilmResults.Items.Clear();
+            try
+            {
+                connection.Open();
+                MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand(query, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    FilmResults.Items.Add($"{reader[3]}, {reader[1]}: {reader[2]}");
+                    //FilmResults.Items.Add($"{reader.GetString("release_year")}, {reader.GetString("title")}: {reader.GetString("description")}");
+                }
+            } 
+            catch (Exception ex) // This is bad practice! Don't catch generic exceptions!
+            {
+                ErrorBox.Text = ex.Message;
+                ErrorBox.Visibility = Visibility.Visible;
+            }
+                finally
+            {
+                connection.Close();
+            }
+
+        }
+        private void SortingMode_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ FilmFinder/README.md
@@ -0,0 +1,13 @@
+# README
+
+This is a simple WPF app that plugs into my local instance of the [Sakila database](https://github.com/jOOQ/sakila/tree/main/mysql-sakila-db) :)
+
+We're doing all our SQL as plain text because we're more interested in using SQL with an app so that you understand the underlying concepts, than we are of you understanding a specific Object-Relational Mapper (ORM).
+
+The aim here is not to download my code and look at it, the aim is to recreate your own without copy-pasting.
+Writing ones' own code:
+- Forces you to read everything line-by-line, character-by-character
+- Makes you have to debug things that you may mis-copy as you go (making and fixing mistakes aids in the learning process)
+- Allows you to personalise your interface ;)
+
+This specifically plugs in with the MySQL Connector -- if you are following along with a different DB outside of the assessments,
