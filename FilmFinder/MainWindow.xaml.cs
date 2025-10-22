ae6aec5fc38dd308e17a87619463f8a2338c4eea
Paraxial O'Kelly <para.okelly@nmtafe.wa.edu.au>
2025-10-22 19:45:46 +0800

    Added rudimentary insert -- using prepared statements

--- FilmFinder/MainWindow.xaml.cs
+++ FilmFinder/MainWindow.xaml.cs
@@ -10,60 +10,89 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml.Linq;
+
 using MySql.Data.MySqlClient;
+
 
 namespace FilmFinder
 {
-    /// <summary>
+        /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    //class Film
+    //{
+    //    private UInt16 _releaseYear;
+    //    private string _name;
+    //    private string _description;
+    //}
+
     public partial class MainWindow : Window
     {
-        private readonly MySql.Data.MySqlClient.MySqlConnection connection;
-        private readonly string dbServer = "127.0.0.1";
-        private readonly string dbPort = "3306";
-        private readonly string dbName = "sakila";
-        private readonly string dbUser = "sakila_app_user";
-        private readonly string dbPassword = "Hunter2!";
+        private readonly MySqlConnection connection;
+        private readonly String dbServer = "127.0.0.1";
+        private readonly String dbPort = "3306";
+        private readonly String dbName = "sakila";
+
+        private readonly String dbUser = "sakila_app_user"; 
+        private readonly String dbPassword = "Hunter2!";
+
         public MainWindow()
         {
             InitializeComponent();
             connection = new MySql.Data.MySqlClient.MySqlConnection();
             connection.ConnectionString = $"server={dbServer};port={dbPort};database={dbName};uid={dbUser};pwd={dbPassword}";
+        }
 
+        private void AddMovie_Click(object sender, RoutedEventArgs e)
+        {
+            ErrorBox.Visibility = Visibility.Hidden;
+            try
+            {
+                connection.Open();
+                string commandString = "insert into film (release_year, title, description, language_id) values (@ryear, @title, @description, 1)";
+                MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand(commandString, connection);
+                command.Parameters.AddWithValue("@ryear", 1979);
+                command.Parameters.AddWithValue("@title", "The Lion of Dessert");
+                command.Parameters.AddWithValue("@description", "A pastry chef hunts down a mysterious confection addicted lion");
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                ErrorBox.Text = ex.Message;
+                ErrorBox.Visibility = Visibility.Visible;
+            } finally
+            {
+                connection.Close();
+            }
         }
 
         private void SearchButton_Click(object sender, RoutedEventArgs e)
         {
-            ErrorBox.Visibility = Visibility.Collapsed;
-            string clause = SearchBar.Text;
-            SearchBar.Text = "";
-
-            string query = $"select * from film where description like '%{clause}%'";
             FilmResults.Items.Clear();
-            try
-            {
+            ErrorBox.Visibility = Visibility.Collapsed;
+            string query = SearchBar.Text;
+            //SearchBarText.Text = "";
+            string defaultQuery = $"select release_year, title, description from film where description like '%{query}%'";
+
+
+            try {
                 connection.Open();
-                MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand(query, connection);
+                MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand(defaultQuery, connection);
                 MySqlDataReader reader = command.ExecuteReader();
 
                 while (reader.Read())
                 {
-                    FilmResults.Items.Add($"{reader[3]}, {reader[1]}: {reader[2]}");
-                    //FilmResults.Items.Add($"{reader.GetString("release_year")}, {reader.GetString("title")}: {reader.GetString("description")}");
+                    //FilmResults.Items.Add($"{reader[0]}, {reader[1]}: {reader[2]}");
+                    FilmResults.Items.Add($"{reader.GetUInt16("release_year")}, {reader.GetString("title")}: {reader.GetString("description")}");
                 }
-            } 
-            catch (Exception ex) // This is bad practice! Don't catch generic exceptions!
-            {
+            } catch (Exception ex) {
                 ErrorBox.Text = ex.Message;
                 ErrorBox.Visibility = Visibility.Visible;
-            }
-                finally
-            {
+            } finally {
                 connection.Close();
             }
-
         }
+
         private void SortingMode_Click(object sender, RoutedEventArgs e)
         {
 
